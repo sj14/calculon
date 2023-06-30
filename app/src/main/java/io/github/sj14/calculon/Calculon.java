55c34e324d83be83e4a8639cdc736f078ae82985
Simon Jürgensmeyer <Simon.Juergensmeyer@gmail.com>
2023-06-30 18:54:18 +0200

    Make it work for Windows and Mac

--- app/src/main/java/io/github/sj14/calculon/Calculon.java
+++ app/src/main/java/io/github/sj14/calculon/Calculon.java
@@ -390,7 +390,7 @@ public class Calculon extends javax.swing.JFrame {
                         expressionsTextPane.getStyledDocument().setCharacterAttributes(charsIndex, expression.length(), styleGray, rootPaneCheckingEnabled);
                     }
 
-                    charsIndex += expressionWithWhitespaces.length();
+                    charsIndex += expressionWithWhitespaces.length() + 2 - System.lineSeparator().length(); // the +2-lineSeperator makes it work for Windows and Mac
                 }
                 resultsTextPane.setText(results.toString());
                 statusBar.setText(String.format("entries: %d sum: %.2f", entries, sum));
